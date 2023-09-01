d3888c75cae9e93066a6f07bc813969101c46e59
Arthur Santos <108244695+ArthurSantosDev@users.noreply.github.com>
2023-09-01 23:04:58 +0000

    Começando a criar a classe Canvas

--- src/components/scripts/index.js
+++ src/components/scripts/index.js
@@ -1,3 +1,13 @@
+import { Canvas } from "./models/class_canva.js";
+
 (() => {
-    
+    const cnv = document.querySelector('canvas#cnv');
+    const ctx = cnv.getContext('2d');
+
+    const color = document.querySelector('input[type=color]');
+
+    const quadro = new Canvas(cnv, ctx, color.value);
+
+    color.addEventListener('change', () => quadro.setColor(color.value));
+
 })()
--- src/components/scripts/models/class_canva.js
+++ src/components/scripts/models/class_canva.js
@@ -1,3 +1,17 @@
 export class Canvas {
-    
+    can_draw = false;
+    mouse_x = 0;
+    mouse_y = 0;
+
+    constructor (canvas, context, color) {
+        this.cnv = canvas;
+        this.ctx = context;
+        this.color = color;
+    }
+
+    setColor (color_code) {
+        this.color = color_code;
+    }
+
+
 }
