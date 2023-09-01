ecd8bc86910832ddbb68b6c4d55cf7161a79de0f
Arthur Santos <108244695+ArthurSantosDev@users.noreply.github.com>
2023-09-01 20:54:21 -0300

    Update index.js

--- src/components/scripts/index.js
+++ src/components/scripts/index.js
@@ -1,13 +1,19 @@
 import { Canvas } from "./models/class_canva.js";
 
 (() => {
-    const cnv = document.querySelector('canvas#cnv');
-    const ctx = cnv.getContext('2d');
+    document.addEventListener('DOMContentLoaded', () => {
+        const cnv = document.querySelector('canvas#cnv');
+        const ctx = cnv.getContext('2d');
+        const color = document.querySelector('input[type=color]');
 
-    const color = document.querySelector('input[type=color]');
+        const quadro = new Canvas(cnv, ctx, color.value);
 
-    const quadro = new Canvas(cnv, ctx, color.value);
+        color.addEventListener('change', () => quadro.setColor(color.value));
 
-    color.addEventListener('change', () => quadro.setColor(color.value));
-
-})()
+        cnv.addEventListener('mousedown', quadro.mouseDown.bind(quadro));
+        cnv.addEventListener('mousemove', quadro.mouseMove.bind(quadro));
+        cnv.addEventListener('mouseup', quadro.mouseUp.bind(quadro));
+    
+        document.querySelector('input[type=button]').addEventListener('click', quadro.clearBoard);
+    });
+})();
