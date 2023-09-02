429e2b4652ca6217fba0f8008b0078d0b18d310d
Arthur Santos <108244695+ArthurSantosDev@users.noreply.github.com>
2023-09-01 21:33:19 -0300

    Update index.js

--- src/components/scripts/index.js
+++ src/components/scripts/index.js
@@ -9,11 +9,6 @@ import { Canvas } from "./models/class_canva.js";
         const quadro = new Canvas(cnv, ctx, color.value);
 
         color.addEventListener('change', () => quadro.setColor(color.value));
-
-        cnv.addEventListener('mousedown', quadro.mouseDown.bind(quadro));
-        cnv.addEventListener('mousemove', quadro.mouseMove.bind(quadro));
-        cnv.addEventListener('mouseup', quadro.mouseUp.bind(quadro));
-    
         document.querySelector('input[type=button]').addEventListener('click', quadro.clearBoard);
     });
 })();
