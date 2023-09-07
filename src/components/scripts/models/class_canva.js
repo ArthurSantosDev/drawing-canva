4de577a0db585f29b2e8260bee9229868f4ccac0
Arthur Santos <108244695+ArthurSantosDev@users.noreply.github.com>
2023-09-07 19:11:28 -0300

    Corrigindo erro do traço do canvas

--- src/components/scripts/models/class_canva.js
+++ src/components/scripts/models/class_canva.js
@@ -1,23 +1,13 @@
-/** 
- * Classe que representa o quadro canvas da página
-*/
 export class Canvas {
     can_draw = false;
     drawing_touch = false; // Variável para identificar se o desenho está sendo feito com toque
     last_touch = { x: 0, y: 0 };
 
-    /** Construtor da Classe 
-     * @param { HTMLCanvasElement } canvas Elemento HTML Canvas
-     * @param { number } color Código Hexadecimal da cor da caneta
-     * @param { CanvasRenderingContext2D } context Contexto de Renderização do Canvas (2d)
-    */
     constructor(canvas, context, color) {
         this.cnv = canvas;
         this.ctx = context;
         this.color = color;
 
-        // Adicionando listeners para eventos de toque e de mouse
-        
         this.cnv.addEventListener('mousedown', this.mouseDown.bind(this));
         this.cnv.addEventListener('mousemove', this.mouseMove.bind(this));
         this.cnv.addEventListener('mouseup', this.mouseUp.bind(this));
@@ -27,49 +17,50 @@ export class Canvas {
         this.cnv.addEventListener('touchend', this.touchEnd.bind(this));
     }
 
-    /** Modifica a cor da caneta */
     setColor(color_code) {
         this.color = color_code;
     }
-    
+
     mouseDown(ev) {
         this.can_draw = true;
-        const rect = this.cnv.getBoundingClientRect(); // Obtenha o retângulo do canvas
-    
-        this.mouse_x = ev.clientX - rect.left; // Use clientX e clientY para obter as coordenadas do evento em relação ao canvas
+        const rect = this.cnv.getBoundingClientRect();
+
+        this.mouse_x = ev.clientX - rect.left;
         this.mouse_y = ev.clientY - rect.top;
     }
-    
+
     mouseMove(ev) {
         if (this.can_draw) {
-            const rect = this.cnv.getBoundingClientRect();
-            this.draw(ev.clientX - rect.left, ev.clientY - rect.top);
+            this.draw(ev.clientX, ev.clientY);
         }
     }
-    // ...
-    
+
+    mouseUp(ev) {
+        this.can_draw = false;
+    }
+
     touchStart(ev) {
         ev.preventDefault();
         this.drawing_touch = true;
         const touch = ev.touches[0];
         const rect = this.cnv.getBoundingClientRect();
-    
+
         this.last_touch.x = touch.clientX - rect.left;
         this.last_touch.y = touch.clientY - rect.top;
     }
-    
+
     touchMove(ev) {
         ev.preventDefault();
         if (this.drawing_touch) {
             const touch = ev.touches[0];
             const rect = this.cnv.getBoundingClientRect();
-    
-            this.draw(touch.clientX - rect.left, touch.clientY - rect.top);
+
+            this.draw(touch.clientX, touch.clientY);
             this.last_touch.x = touch.clientX - rect.left;
             this.last_touch.y = touch.clientY - rect.top;
         }
     }
-    
+
     touchEnd(ev) {
         ev.preventDefault();
         this.drawing_touch = false;
@@ -77,36 +68,39 @@ export class Canvas {
 
     draw(x, y) {
         const rect = this.cnv.getBoundingClientRect();
-        let ponto_x = x - rect.left;
-        let ponto_y = y - rect.top;
-    
-        this.ctx.beginPath();
-        this.ctx.lineWidth = 5;
-        this.ctx.lineJoin = 'round';
-    
-        if (this.drawing_touch) {
-            this.ctx.moveTo(this.last_touch.x, this.last_touch.y);
-        } else {
-            this.ctx.moveTo(this.mouse_x, this.mouse_y);
+        const ponto_x = x - rect.left;
+        const ponto_y = y - rect.top;
+
+        if (this.can_draw || this.drawing_touch) {
+            this.ctx.lineWidth = 5;
+            this.ctx.lineJoin = 'round';
+            this.ctx.lineCap = 'round';
+            this.ctx.strokeStyle = this.color;
+
+            this.ctx.beginPath();
+
+            if (this.drawing_touch) {
+                this.ctx.moveTo(this.last_touch.x, this.last_touch.y);
+            } else {
+                this.ctx.moveTo(this.mouse_x, this.mouse_y);
+            }
+
+            this.ctx.lineTo(ponto_x, ponto_y);
+            this.ctx.stroke();
+            this.ctx.closePath();
         }
-    
-        this.ctx.lineTo(ponto_x, ponto_y);
-        this.ctx.closePath();
-        this.ctx.strokeStyle = this.color;
-        this.ctx.stroke();
-    
-        if (this.drawing_touch) {
-            this.last_touch.x = ponto_x;
-            this.last_touch.y = ponto_y;
-        } else {
+
+        this.last_touch.x = ponto_x;
+        this.last_touch.y = ponto_y;
+
+        if (!this.drawing_touch) {
             this.mouse_x = ponto_x;
             this.mouse_y = ponto_y;
         }
     }
 
-    /** Função que limpa o quadro canvas */
-    clearBoard () {
+    clearBoard() {
         this.ctx.setTransform(1, 0, 0, 1, 0, 0);
-        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.ctx.clearRect(0, 0, this.cnv.width, this.cnv.height);
     }
 }
