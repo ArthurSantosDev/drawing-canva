d5e88452b0afbc434bfc7b239924be2518b44dbb
Arthur Santos <108244695+ArthurSantosDev@users.noreply.github.com>
2023-09-07 19:21:32 -0300

    Update class_canva.js

--- src/components/scripts/models/class_canva.js
+++ src/components/scripts/models/class_canva.js
@@ -34,8 +34,8 @@ export class Canvas {
 
     mouseDown(ev) {
         this.can_draw = true;
-        this.mouse_x = ev.pageX - (this.cnv.offsetLeft + 50);
-        this.mouse_y = ev.pageY - (this.cnv.offsetTop + 50);
+        this.mouse_x = ev.clientX;
+        this.mouse_y = ev.clientY;
     }
 
     mouseMove(ev) {
@@ -72,8 +72,8 @@ export class Canvas {
     }
 
     draw(x, y) {
-        let ponto_x = x - (this.cnv.offsetLeft + 50);
-        let ponto_y = y - (this.cnv.offsetTop + 50);
+        let ponto_x = x ;
+        let ponto_y = y;
 
         this.ctx.beginPath();
         this.ctx.lineWidth = 5;
